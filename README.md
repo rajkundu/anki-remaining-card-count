f6be795bbc16adbd2ac8139aabbd471f292d5d7c
Raj Kundu <rjk368@gmail.com>
2025-01-18 18:45:17 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Toggle Remaining Card Counts
+
+![Screenshot of remaining card count in Anki](remaining_card_count.png)
+
+This add-on allows you to toggle visibility of the Remaining Card Count (pictured above) using either a keyboard shortcut or an Anki Menu Bar item.
+
+The default keyboard shortcut is the backtick/grave accent key (`), which is to the left of the number "1" on the US QWERTY keyboard. This shortcut can be configured in the add-on's config JSON.
+
+### Example Add-On Configuration
+To change the keyboard shortcut, open Tools > Add-ons, then select this add-on and click the "Config" button. For example, the following configuration:
+```json
+{
+    "shortcut": "Ctrl+Shift+8"
+}
+```
+will change the keyboard shortcut to `Ctrl`+`Shift`+`8` (`Cmd`+`Shift`+`8` on Mac).
---
+++ __init__.py
@@ -0,0 +1,29 @@
+from aqt import mw
+from PyQt6.QtGui import QAction, QKeySequence
+from PyQt6.QtCore import Qt
+from aqt.qt import qconnect
+from aqt.operations.collection import set_preferences
+from aqt.utils import showInfo
+
+def toggle_remaining_due_counts():
+    # Derived from https://github.com/ankitects/anki/blob/1be94a8b0461defcbf2a2d39e5a1f316446b0169/qt/aqt/preferences.py#L153
+    prefs = mw.col.get_preferences()
+    prefs.reviewing.show_remaining_due_counts = not prefs.reviewing.show_remaining_due_counts
+    set_preferences(parent=mw, preferences=prefs).run_in_background()
+
+def init():
+    config = mw.addonManager.getConfig(__name__)
+
+    key_seq = QKeySequence('`')
+    if config is not None:
+        if "shortcut" in config and config['shortcut']:
+            key_seq = QKeySequence(config['shortcut'])
+            if key_seq.matches(Qt.Key.Key_unknown) != QKeySequence.SequenceMatch.NoMatch:
+                showInfo(f"WARNING: The configured keyboard shortcut for the Toggle Remaining Card Counts add-on is invalid! Please double-check the configuration under Tools > Add-Ons.")
+
+    action = QAction("Toggle Remaining Card Counts", mw)
+    qconnect(action.triggered, toggle_remaining_due_counts)
+    action.setShortcut(QKeySequence(key_seq))
+    mw.form.menuTools.addAction(action)
+
+init()
