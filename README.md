8726f4f321e19b3ad78a4b58bf319967cac48408
Raj Kundu <rjk368@gmail.com>
2025-01-20 00:40:18 -0500

    combine web and preferences methods

--- README.md
+++ README.md
@@ -13,20 +13,7 @@ The default keyboard shortcut is the backtick/grave accent key (`), which is to 
 To change the keyboard shortcut, open Tools > Add-ons, then select this add-on and click the "Config" button. For example, the following configuration:
 ```json
 {
-    "method": "preferences",
     "shortcut": "Ctrl+Shift+8"
 }
 ```
 will change the keyboard shortcut to `Ctrl`+`Shift`+`8` (`Cmd`+`Shift`+`8` on Mac).
-
-### Toggling Method (`method`)
-By default, this add-on works by toggling the built-in Anki preference for showing the Remaining Card Count. Each time the preference is changed, Anki refreshes the current deck. This can cause odd behavior, such as a Learning-phase card being pushed to the front of the deck and replacing a Review-phase card that was just on the screen.<br>
-<br>
-This add-on can circumvent this behavior by showing/hiding the Remaining Card Count using a web-based method instead of using Anki preferences. To enable this, use `"method": "web"` in the add-on's config, e.g.:
-```json
-{
-    "method": "web",
-    "shortcut": "`"
-}
-```
-NOTE: If using the `web` method, then **you must <u>enable</u> the Anki Preference for the showing Remaining Card Count</u>**!
--- __init__.py
+++ __init__.py
@@ -5,33 +5,45 @@ from aqt.qt import qconnect
 from aqt.operations.collection import set_preferences
 from aqt.utils import showInfo
 
-def toggle_count_via_preferences():
+from aqt import operations
+from aqt.operations import ResultWithChanges
+from concurrent.futures._base import Future
+
+def toggle_count():
+    # Only support toggling when a question/card is on-screen (ensure ease buttons are present)
+    if mw.reviewer.state != "question" or not mw.reviewer.card:
+        return
+
     # Derived from https://github.com/ankitects/anki/blob/1be94a8b0461defcbf2a2d39e5a1f316446b0169/qt/aqt/preferences.py#L153
     prefs = mw.col.get_preferences()
     prefs.reviewing.show_remaining_due_counts = not prefs.reviewing.show_remaining_due_counts
-    set_preferences(parent=mw, preferences=prefs).run_in_background()
+    collection_op = set_preferences(parent=mw, preferences=prefs)
 
-def toggle_count_via_web():
-    mw.bottomWeb.eval("""
-        var styleTag = document.getElementById("remainingCardCountStyle");
-        if (styleTag) {
-            const newVisibility = styleTag.innerHTML.includes('hidden') ? 'visible' : 'hidden';
-            styleTag.innerHTML = `
-                .stattxt {
-                    visibility: ${newVisibility};
-                }
-            `;
-        } else {
-            styleTag = document.createElement("style");
-            styleTag.id = "remainingCardCountStyle";
-            styleTag.innerHTML = `
-                .stattxt {
-                    visibility: hidden; /* hidden because toggle has been called w/ the count initially being visible */
-                }
-            `;
-            document.body.appendChild(styleTag);
-        }
-    """)
+    # Derived from https://github.com/ankitects/anki/blob/64ca90934bc26ddf7125913abc9dd9de8cb30c2b/qt/aqt/operations/__init__.py#L99
+    # Redraws bottom bar ONLY
+    assert mw
+    mw._increase_background_ops()
+
+    def wrapped_op() -> ResultWithChanges:
+        assert mw
+        return collection_op._op(mw.col)
+
+    def wrapped_done(future: Future) -> None:
+        assert mw
+        mw._decrease_background_ops()
+        # did something go wrong?
+        if exception := future.exception():
+            if isinstance(exception, Exception):
+                operations.show_exception(parent=collection_op._parent, exception=exception)
+                return
+            else:
+                # BaseException like SystemExit; rethrow it
+                future.result()
+
+        result = future.result()
+        mw.reviewer._showAnswerButton()
+
+    collection_op._run(mw, wrapped_op, wrapped_done)
 
 def init():
     config = mw.addonManager.getConfig(__name__)
@@ -43,19 +55,8 @@ def init():
             showInfo(f"WARNING: The configured keyboard shortcut for the Toggle Remaining Card Count add-on is invalid! Please double-check the configuration under Tools > Add-Ons.")
             return
 
-    toggle_remaining_card_count = toggle_count_via_preferences # default
-    if config is not None and "method" in config and config['method']:
-        method = config['method'].lower().strip()
-        if method == "web":
-            toggle_remaining_card_count = toggle_count_via_web
-        elif method == "preferences":
-            pass # this is already the default
-        else:
-            showInfo(f"WARNING: 'method' for the Toggle Remaining Card Count add-on must be one of ('web', 'preferences')! Please double-check the configuration under Tools > Add-Ons.")
-            return
-
     action = QAction("Toggle Remaining Card Count", mw)
-    qconnect(action.triggered, toggle_remaining_card_count)
+    qconnect(action.triggered, toggle_count)
     action.setShortcut(QKeySequence(key_seq))
     mw.form.menuTools.addAction(action)
 
